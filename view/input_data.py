144f703c0a95ce522ad1cdc4631a31c7c2f49d86
TeleqKD4 <vitoalviano15@gmail.com>
2025-01-04 12:37:02 +0700

    Update input_data.py

--- view/input_data.py
+++ view/input_data.py
@@ -1,1 +1,17 @@
-
+```
+class inputakun:
+    @staticmethod
+    def input_akun():
+        akun = input(" Masukan Nama Program ")
+        username = input (" Masukan Nama Akun Program ")
+        password = input (" Masukan Pasword Program")
+        return akun, username, password
+        
+    @staticmethod
+    def input_edit_akun():
+        akun = input (" Masukan Nama Program")
+        print(" Masukan Data Baru ( Tekan Enter Jika Tidak Ingin Mengubah Data ): ")
+        username = input(" Masukan Username Akum: ")
+        password = input(" Masukan Password Akun ")
+        return akun, username, password
+```
